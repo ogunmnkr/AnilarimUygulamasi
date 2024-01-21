71e3e6af37aaa030a58138e31019d130923b3b15
Ogün Minkara <101428103+ogunmnkr@users.noreply.github.com>
2024-01-21 12:28:25 +0300

    Kayıt ol
    main.storyboarda yeni vc eklendi

---
+++ NotlarimUygulamasi/NotlarimUygulamasi/kayit.swift
@@ -0,0 +1,26 @@
+//
+//  kayit.swift
+//  NotlarimUygulamasi
+//
+//  Created by Ogün Minkara on 21.01.2024.
+//
+
+import UIKit
+import Firebase
+
+class kayit: UIViewController {
+    @IBOutlet weak var mailText: UITextField!
+    
+    @IBOutlet weak var sifreText: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+
+    @IBAction func kayitOlButonu(_ sender: Any) {
+    }
+}
